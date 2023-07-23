d95d258792ccf261550a74da34522aab74a984bc
PiroAvni <avnipiro.dev@gmail.com>
2023-07-23 21:24:30 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Wrapper from "./sections/Wrapper";
+import Footer from "./sections/Footer";
+import Background from "./components/Background";
+import './scss/index.scss';
+
+
+
+
+const App = () => {
+  return <div className="main-container">
+    <Background/>
+    <div className="app">
+      <Navbar/>
+      <Wrapper/>
+      <Footer/>
+    </div>
+  </div>;
+};
+
+export default App;
